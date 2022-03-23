25bb2aa952c8370cc196aa2260fac5d3b0714958
Michael Hansen <hansen.mike@gmail.com>
2022-03-23 19:46:55 +0000

    Tighten relative date intent to avoid catching "when XXX" queries

--- __init__.py
+++ __init__.py
@@ -74,7 +74,7 @@ class DateSkill(MycroftSkill):
             self._handle_current_date()
 
     @intent_handler(
-        AdaptIntent().require("query").require("relative-day").optionally("date")
+        AdaptIntent().require("query").require("relative-day").require("date")
     )
     def handle_relative_date_request(self, request: Message):
         """Respond to a request from the user for a date in the past or future.
